2918493b3f64829213579350a7c69882e9c73f43
Erik <erik.grundy@gmail.com>
2022-01-13 01:18:05 +1300

    Restructured main file; added `all` option

--- Program.fs
+++ Program.fs
@@ -1,29 +1,35 @@
 ﻿module Program
 
-[<EntryPoint>]
-let main args = 
-    let day = int args[0]
-    let (f, file) =
-        match day with
-            | 1 -> (Day1.main, "resources/1.txt")
-            | 2 -> (Day2.main, "resources/2.txt")
-            | 3 -> (Day3.main, "resources/3.txt")
-            | 4 -> (Day4.main, "resources/4.txt")
-            | 5 -> (Day5.main, "resources/5.txt")
-            | 6 -> (Day6.main, "resources/6.txt")
-            | 7 -> (Day7.main, "resources/7.txt")
-            | 8 -> (Day8.main, "resources/8.txt")
-            | 9 -> (Day9.main, "resources/9.txt")
-            | 10 -> (Day10.main, "resources/10.txt")
-            | 11 -> (Day11.main, "resources/11.txt")
-            | 12 -> (Day12.main, "resources/12.txt")
-            | 13 -> (Day13.main, "resources/13.txt")
-            | 14 -> (Day14.main, "resources/14.txt")
-            | 15 -> (Day15.main, "resources/15.txt")
-            | 16 -> (Day16.main, "resources/16.txt")
-            | 17 -> (Day17.main, "resources/17.txt")
-            | 18 -> (Day18.main, "resources/18.txt")
-            | _ -> failwith "day not implemented yet"
-    let input = System.IO.File.ReadLines file
+let runDay (f, input) =
+    let input = System.IO.File.ReadLines input
     f input
+
+[<EntryPoint>]
+let main args =
+    let days = [|
+        (Day1.main, "resources/1.txt")
+        (Day2.main, "resources/2.txt")
+        (Day3.main, "resources/3.txt")
+        (Day4.main, "resources/4.txt")
+        (Day5.main, "resources/5.txt")
+        (Day6.main, "resources/6.txt")
+        (Day7.main, "resources/7.txt")
+        (Day8.main, "resources/8.txt")
+        (Day9.main, "resources/9.txt")
+        (Day10.main, "resources/10.txt")
+        (Day11.main, "resources/11.txt")
+        (Day12.main, "resources/12.txt")
+        (Day13.main, "resources/13.txt")
+        (Day14.main, "resources/14.txt")
+        (Day15.main, "resources/15.txt")
+        (Day16.main, "resources/16.txt")
+        (Day17.main, "resources/17.txt")
+        (Day18.main, "resources/18.txt")
+    |]
+    if args.[0] = "all" then days |> Array.iter runDay
+    else
+        let day = int args.[0]
+        match Array.tryItem (day - 1) days with
+        | Some pair -> runDay pair
+        | None -> printfn "Day %d isn't implemented yet" day
     0
