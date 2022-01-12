47e038329d6a13d27dd3796debc01910ab268f57
Erik <erik.grundy@gmail.com>
2022-01-13 00:55:30 +1300

    Moved things around

--- src/Day18.fs
+++ src/Day18.fs
@@ -1,87 +1,8 @@
 module Day18
 
-let undefined () = failwith "undefined"
+open Utils.Parse
 
-let first f (a, b) = (f a, b)
-
-let second f (a, b) = (a, f b)
-
-[<AutoOpenAttribute>]
-module Parse =
-    type 'a Parser = P of (string -> ('a * string) option)
-
-    let parse (P p) s = p s
-
-    let (<?>) f (P p) = P (fun s -> p s |> Option.map (first f))
-
-    let ret a = P (fun s -> Some (a, s))
-
-    let (>>=?) (P pa: 'a Parser) (pm: ('a -> 'b Parser)): 'b Parser =
-        P (fun s -> pa s |> Option.bind (fun (a, rest) -> parse (pm a) rest))
-
-    let (>>?) l p = l >>=? (fun _ -> p)
-
-    let (<<?) p r = p >>=? (fun v -> r >>? (ret v))
-
-    let (<*>?) pf pa = pf >>=? (fun f -> f <?> pa)
-
-    let (<|>?) (P p1) (P p2) = P (fun s -> Option.orElse (p2 s) (p1 s))
-
-    let map = (<?>)
-
-    let join = (<*>?)
-
-    let bind pm pa = pa >>=? pm
-
-    let alt = (<|>?)
-
-    let longest (P p1) (P p2) =
-        P (fun s ->
-            match (p1 s, p2 s) with
-            | (None, None) -> None
-            | (None, Some v2) -> Some v2
-            | (Some v1, None) -> Some v1
-            | (Some v1, Some v2) ->
-                Some (Seq.minBy (snd >> Seq.length) [v1; v2]))
-
-    let rec sequence parsers =
-        match parsers with
-        | [] -> ret []
-        | p :: ps -> ((fun v vs -> v :: vs) <?> p) <*>? (sequence ps)
-
-    let get: char Parser =
-        P (fun s -> if s = "" then None else Some(s[0], s[1..]))
-
-    let peek: char Parser =
-        P (fun s -> if s = "" then None else Some(s[0], s))
-    
-    let charP c = P (fun s -> parse get s |> Option.filter (fst >> (=) c))
-
-    let stringP (s: string) =
-        s
-        |> Seq.map charP
-        |> Seq.toList
-        |> sequence
-        |> map System.String.Concat
-
-    let intP = P (fun s ->
-        let intPart = s |> Seq.takeWhile Utils.isDigit |> System.String.Concat
-        let rest = s.Substring(intPart.Length)
-        if intPart = "" then None else Some(int64 intPart, rest))
-
-    let surround pl pr p = pl >>? p <<? pr
-
-    // Parses 0 or more of `p`, and returns the list of all parsed values
-    let rec many (p: 'a Parser): 'a list Parser = P (fun s ->
-        match parse p s with
-        | Some (x, s') ->
-            parse (many p) s' |> Option.map (fun (xs, s'') -> (x :: xs, s''))
-        | None -> Some ([], s))
-
-    let many1 p = (List.insertAt 0) <?> p <*>? many p
-
-    let ws = many1 (charP ' ' <|>? charP '\t' <|>? charP '\n') >>? ret ()
-
+#nowarn "40"
 let part1 lines =
     let rec exprParser = 
         let recur = P (fun s -> parse exprParser s)
@@ -94,20 +15,17 @@ let part1 lines =
     lines |> Seq.map (parse exprParser >> Option.get >> fst) |> Seq.sum
 
 let findMatching (s: string) (n: int): int =
-    if s[n] <> '(' then
-        failwith (sprintf "character under cursor must be `(`: `%s`, %i" s n)
-    else
-        let mutable depth = 1
-        let mutable i = n + 1
-        let finish = s.Length
-        while i < finish && depth > 0 do
-            if s[i] = '(' then depth <- depth + 1
-            if s[i] = ')' then depth <- depth - 1
-            i <- i + 1
-        if s[i - 1] = ')' then
-            (i - 1)
+    let rec find depth position =
+        if position > s.Length then failwith "no matching parenthesis"
         else
-            failwith (sprintf "couldn't find matching parenthesis: %s %d" s n)
+            let depth' =
+                match s[position] with
+                | '(' -> depth + 1
+                | ')' -> depth - 1
+                | _ -> depth
+            if depth' = 0 then position else find depth' (position + 1)
+    if s[n] <> '(' then failwith "character under cursor must be `(`"
+    else find 1 (n + 1)
 
 let part2 lines =
     let rec eval (s: string): int64 =
--- src/Utils.fs
+++ src/Utils.fs
@@ -1,8 +1,26 @@
 module Utils
 
+// Sketching out various things
+// ----------------------------
+
+let undefined () = failwith "undefined"
+
+// Tuples
+// ------
+
+let first f (a, b) = (f a, b)
+
+let second f (a, b) = (a, f b)
+
+// Character predicates
+// --------------------
+
 let isDigit c = 48 <= int c && int c <= 57
 
 let isHexDigit c = isDigit c || Seq.contains c "abcdef"
+
+// Dealing with lists
+// ------------------
 
 let rec split x xs =
     if Seq.isEmpty xs then
@@ -15,6 +33,9 @@ let rec split x xs =
         else
             seq { yield head; yield! (split x (Seq.tail tail)) }
 
+// Iterating
+// ---------
+
 let rec iterate f x = seq { yield x; yield! (iterate f (f x))}
 
 let converge step =
@@ -24,3 +45,81 @@ let converge step =
     >> Seq.head
     >> snd
 
+// Parsing
+// ------
+
+[<AutoOpenAttribute>]
+module Parse =
+    type 'a Parser = P of (string -> ('a * string) option)
+
+    let parse (P p) s = p s
+
+    let (<?>) f (P p) = P (fun s -> p s |> Option.map (first f))
+
+    let ret a = P (fun s -> Some (a, s))
+
+    let (>>=?) (P pa: 'a Parser) (pm: ('a -> 'b Parser)): 'b Parser =
+        P (fun s -> pa s |> Option.bind (fun (a, rest) -> parse (pm a) rest))
+
+    let (>>?) l p = l >>=? (fun _ -> p)
+
+    let (<<?) p r = p >>=? (fun v -> r >>? (ret v))
+
+    let (<*>?) pf pa = pf >>=? (fun f -> f <?> pa)
+
+    let (<|>?) (P p1) (P p2) = P (fun s -> Option.orElse (p2 s) (p1 s))
+
+    let map = (<?>)
+
+    let join = (<*>?)
+
+    let bind pm pa = pa >>=? pm
+
+    let alt = (<|>?)
+
+    let longest (P p1) (P p2) =
+        P (fun s ->
+            match (p1 s, p2 s) with
+            | (None, None) -> None
+            | (None, Some v2) -> Some v2
+            | (Some v1, None) -> Some v1
+            | (Some v1, Some v2) ->
+                Some (Seq.minBy (snd >> Seq.length) [v1; v2]))
+
+    let rec sequence parsers =
+        match parsers with
+        | [] -> ret []
+        | p :: ps -> ((fun v vs -> v :: vs) <?> p) <*>? (sequence ps)
+
+    let get: char Parser =
+        P (fun s -> if s = "" then None else Some(s[0], s[1..]))
+
+    let peek: char Parser =
+        P (fun s -> if s = "" then None else Some(s[0], s))
+    
+    let charP c = P (fun s -> parse get s |> Option.filter (fst >> (=) c))
+
+    let stringP (s: string) =
+        s
+        |> Seq.map charP
+        |> Seq.toList
+        |> sequence
+        |> map System.String.Concat
+
+    let intP = P (fun s ->
+        let intPart = s |> Seq.takeWhile isDigit |> System.String.Concat
+        let rest = s.Substring(intPart.Length)
+        if intPart = "" then None else Some(int64 intPart, rest))
+
+    let surround pl pr p = pl >>? p <<? pr
+
+    // Parses 0 or more of `p`, and returns the list of all parsed values
+    let rec many (p: 'a Parser): 'a list Parser = P (fun s ->
+        match parse p s with
+        | Some (x, s') ->
+            parse (many p) s' |> Option.map (fun (xs, s'') -> (x :: xs, s''))
+        | None -> Some ([], s))
+
+    let many1 p = (List.insertAt 0) <?> p <*>? many p
+
+    let ws = many1 (charP ' ' <|>? charP '\t' <|>? charP '\n') >>? ret ()
