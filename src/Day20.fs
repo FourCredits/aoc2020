0913d0b3a848f2420e55c6d67ba5e9dcf92987bc
Erik <erik.grundy@gmail.com>
2022-02-06 23:08:26 +1300

    Day 20: part 1. No idea how I'll do part 2

--- src/Day20.fs
+++ src/Day20.fs
@@ -17,6 +17,55 @@ let parse lines =
         Tile (idNumber, image)
     lines |> Utils.split "" |> Seq.map (Seq.toArray >> tile)
 
+// Note: only works on square images
+let rotate (image: bool array array) =
+    let size = Array.length image
+    [| for i = 0 to size - 1 do
+        [| for j = 0 to size - 1 do
+            image[size - j - 1][i] |] |]
+
+let flip (image: bool array array) =
+    let size = Array.length image
+    [| for i = 0 to size - 1 do
+        [| for j = 0 to size - 1 do
+            image[size - i - 1][j] |] |]
+
+let rotations original =
+    let r1 = rotate original
+    let r2 = rotate r1
+    let r3 = rotate r2
+    [ original; r1; r2; r3 ]
+
+let orientations (Tile (id, image)): Tile list =
+    rotations image @ rotations (flip image)
+    |> List.map (fun i -> Tile (id, i))
+
+let leftEdge   (Tile (id, image)): bool array = Array.map Array.head image
+let rightEdge  (Tile (id, image)): bool array = Array.map Array.last image
+let topEdge    (Tile (id, image)): bool array = Array.head image
+let bottomEdge (Tile (id, image)): bool array = Array.last image
+
+let lineUpVertical   tile1 tile2 = rightEdge  tile1 = leftEdge tile2
+let lineUpHorizontal tile1 tile2 = bottomEdge tile1 = topEdge  tile2
+
+let lineUp tile1 tile2 =
+    let lineUp' (tile1, tile2) =
+        lineUpVertical tile1 tile2 || lineUpVertical tile2 tile1 ||
+        lineUpHorizontal tile1 tile2 || lineUpHorizontal tile2 tile1
+    (orientations tile1, orientations tile2)
+    ||> List.allPairs
+    |> List.exists lineUp'
+
+let part1 (tiles: Tile seq) =
+    [ for (Tile (id, _)) as t1 in tiles do
+        Seq.except [t1] tiles
+        |> Seq.filter (fun t2 -> lineUp t1 t2)
+        |> Seq.length
+        |> (fun n -> (id, n))]
+    |> List.filter (snd >> ((=) 2))
+    |> List.map fst
+    |> List.reduce (*)
+
 let corners (grid: 'a array array): 'a array =
     let yMax = Array.length grid
     let xMax = Array.length grid[0]
@@ -132,4 +181,4 @@ Tile 3079:
 
 let main lines =
     let parsed = parse lines
-    printfn "%A" (parsed |> Seq.length)
+    printfn $"{part1 parsed}"
