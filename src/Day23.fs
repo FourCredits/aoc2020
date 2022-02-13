1a3c9f380c2fd530be2638199ca5082f29ee73a4
Erik <erik.grundy@gmail.com>
2022-02-13 17:34:09 +1300

    Day 23, part 2: changed everything - see src/Day23.fs for more details

--- src/Day23.fs
+++ src/Day23.fs
@@ -1,51 +1,78 @@
 module Day23
 
-open System.Diagnostics
+open System.Collections.Generic
 
-let rec findDestinationCup cups currentCup =
-    // match Array.tryFindIndex ((=) (currentCup - 1)) cups with
-    // | Some x -> x
-    // | None ->
-    //     let currentCup' = currentCup - 1
-    //     if Array.forall ((<) currentCup') cups then
-    //         Array.findIndex ((=) (Array.max cups)) cups
-    //     else
-    //         findDestinationIndex cups currentCup'
-    [(currentCup - 1) .. -1 .. 1]
-    |> Seq.filter (fun cup -> Array.contains cup cups)
-    |> Seq.tryHead
-    |> Option.defaultValue (Array.max cups)
+(*
+ *  Big takeaways from this one:
+ *
+ *  - Don't be afraid of big numbers. I saw the numbers 1000000 and 10000000,
+ *    and figured that the problem was to find a loop, or do some memoization.
+ *    Turns out, it's easier to just go through all 10 million iterations of a
+ *    1 million size list, so long as you do it correctly
+ *  - Linked lists are really useful for these times when you are rearranging a
+ *    structure while keeping basically the same order. The solutions I came up
+ *    with had a whole lot of unnecessary copying. While I could get away with
+ *    that when the list was 9 elements long, you absolutely can't do that when
+ *    the list is a million times.
+ *  - Dictionaries are a good way of creating a linked list when you want to do
+ *    the base manipulations of a list (which you don't really have access to
+ *    on the in-built List).
+ *)
 
-let solve iterations startingCups =
-    let mutable cups = startingCups
-    let mutable currentCup = Array.head startingCups
-    for i = 1 to iterations do
-        let currentIndex = Array.findIndex ((=) currentCup) cups
-        let threeCups =
-            [| 1 .. 3 |]
-            |> Array.map (fun i -> cups[(currentIndex + i) % (Array.length startingCups)])
-        cups <- Array.filter (fun cup -> not (Array.contains cup threeCups)) cups
-        let destinationCup = findDestinationCup cups currentCup
-        let destinationIndex = Array.findIndex ((=) destinationCup) cups
-        cups <- Array.insertManyAt (destinationIndex + 1) threeCups cups
-        let currentIndex = Array.findIndex ((=) currentCup) cups
-        currentCup <- cups[(currentIndex + 1) % Array.length cups]
-    cups
+let prepareLinkedList (cups: int array) (fullLength: int) =
+    let numCups = Array.length cups
+    [0 .. fullLength - 1]
+    |> Seq.map (fun i ->
+        if i < numCups - 1 then
+            (cups[i], cups[i + 1])
+        elif i = numCups - 1 && numCups = fullLength then
+            (cups[i], cups[0])
+        elif i = numCups - 1 && numCups < fullLength then
+            (cups[i], Array.max cups + 1)
+        elif i = fullLength - 1 then
+            (i + 1, cups[0])
+        else
+            (i + 1, i + 2)
+        |> KeyValuePair)
+    |> Dictionary
 
-let part1 startingCups =
-    let finalCups = solve 100 startingCups
-    finalCups
-    |> Array.splitAt (Array.findIndex ((=) 1) finalCups)
-    |> fun (a, b) -> Array.append (Array.tail b) a
-    |> Array.map string
-    |> String.concat ""
-    |> int
+let solveGame (cups: int array) (fullLength: int) (numMoves: int) =
+    let cuplist = prepareLinkedList cups fullLength
+    let wrap n =
+        let rec f n d = if n < 0 then f (n + d) d else n % d
+        (f (n - 2) fullLength) + 1
+    let mutable ptr = cups[0]
+    for _ = 1 to numMoves do
+        // remove three cups
+        let c1 = cuplist[ptr]
+        let c2 = cuplist[c1]
+        let c3 = cuplist[c2]
+        cuplist[ptr] <- cuplist[c3]
+        // find dest
+        let dest =
+            Utils.iterate wrap (wrap ptr)
+            |> Seq.find (fun i -> i <> c1 && i <> c2 && i <> c3)
+        // reinsert cups after dest
+        cuplist[c3] <- cuplist[dest]
+        cuplist[dest] <- c1
+        // advance ptr
+        ptr <- cuplist[ptr]
+    cuplist
+
+let part1 cups =
+    let solved = solveGame cups (Array.length cups) 100
+    Utils.iterate (fun x -> solved[x]) solved[1]
+    |> Seq.takeWhile ((<>) 1)
+    |> Seq.fold (fun acc i -> (acc * 10) + i) 0
+
+let part2 cups =
+    let finalCups = solveGame cups 1000000 10000000
+    (int64 finalCups[1]) * (int64 finalCups[finalCups[1]])
 
 let main lines =
     let parsed =
         lines
-        // [| "389125467" |]
         |> Array.exactlyOne
         |> Seq.map (fun (c: char) -> int c - 48)
         |> Array.ofSeq
-    printfn $"{part1 parsed}"
+    printfn $"{part1 parsed}, {part2 parsed}"
